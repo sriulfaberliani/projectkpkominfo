d014a8f78a7e55bce478ecba243d28a186d10c2e
NadillaSaraswati <90832058+NadillaSaraswati@users.noreply.github.com>
2023-01-11 07:43:24 +0700

    haha

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,15 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+
+        $data = [
+            'title' => 'Dashboard'
+        ];
+
+        echo view('templates/v_header', $data);
+        echo \view('templates/v_sidebar');
+        echo view('templates/v_topbar');
+        echo view('home/index');
+        echo view('templates/v_footer');
     }
 }
