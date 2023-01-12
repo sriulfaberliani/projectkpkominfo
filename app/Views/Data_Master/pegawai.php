b91db1cafc0c0d8d64e65edf58a0a88d73b47e26
sriulfaberliani <sriulfaberliani2002@gmail.com>
2023-01-12 08:24:52 +0700

    modal tambah

--- app/Views/Data_Master/pegawai.php
+++ app/Views/Data_Master/pegawai.php
@@ -7,14 +7,12 @@
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-    <a href="#" class="btn btn-primary btn-icon-split">
-                                <span class="icon text-white-50">
+    <button type="button" class="btn btn-primary btn-icon-split" data-toggle="modal" data-target="#modalTambah">
+    <span class="icon text-white-50">
                                     <i class="fas fa-plus"></i>
                                 </span>
                                 <span class="text">Tambah Data</span>
-                            </a>
-    
-    </button>
+               </button>
     </div>
     <div class="card-body">
         <div class="table-responsive">
@@ -120,3 +118,45 @@
 
 </div>
 <!-- End of Main Content -->
+
+<div class="modal fade" id="modalTambah" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                   <div class="modal-dialog" role="document">
+                       <div class="modal-content">
+                           <div class="modal-header">
+                               <h5 class="modal-title">Tambah</h5>
+                                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                       <span aria-hidden="true">&times;</span>
+                                   </button>
+                           </div>
+                           <div class="modal-body">
+                               <form action="" method="post"> 
+                               <div class="form-group ab-0 ab-0">
+                                 <label for="id_anggota"></label>
+                                 <input type="text" name="id_anggota" id="id_anggota" class="form-control" placeholder="Masukkan Id Anggota" >
+                               </div>
+                               <div class="form-group ab-0">
+                                 <label for="no_registrasi"></label>
+                                 <input type="text" name="no_registrasi" id="no_registrasi" class="form-control" placeholder="Masukkan No Registrasi" >
+                               </div>
+                               <div class="form-group ab-0">
+                                 <label for="id_jurusan"></label>
+                                 <input type="text" name="id_jurusan" id="id_jurusan" class="form-control" placeholder="Masukkan Id Jurusan" >
+                               </div>
+                               <div class="form-group ab-0">
+                                 <label for="nama_anggota"></label>
+                                 <input type="text" name="nama_anggota" id="nama_anggota" class="form-control" placeholder="Masukkan Nama Anggota" >
+                               </div>
+                               <div class="form-group ab-0">
+                                 <label for="no_induk"></label>
+                                 <input type="text" name="no_induk" id="no_induk" class="form-control" placeholder="Masukkan No Induk Anggota" >
+                               </div>
+                               
+                           </div>
+                           <div class="modal-footer">
+                               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                               <button type="submit" class="btn btn-primary">Tambah Data</button>
+                           </div>
+                           </form>
+                       </div>
+                   </div>
+               </div>
