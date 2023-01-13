6d08c6f3196aca3c617120655e6896c68502e736
sriulfaberliani <sriulfaberliani2002@gmail.com>
2023-01-13 12:14:42 +0700

    jjhbceubcueyc

--- app/Controllers/DataPegawai.php
+++ app/Controllers/DataPegawai.php
@@ -18,7 +18,7 @@ class DataPegawai extends BaseController
 
         $data = [
             'title' => 'Pegawai',
-            'pegawai' => $this->model->getAllData()
+            'datapegawai' => $this->model->getAllData()
         ];
 
         echo view('templates/v_header', $data);
@@ -27,4 +27,60 @@ class DataPegawai extends BaseController
         echo view('Data_Master/pegawai', $data);
         echo view('templates/v_footer');
     }
+
+    public function tambah()
+    {
+        $data = [
+            // 'id_pegawai' => $this->request->getPost('id_pegawai'),
+            'nama_pegawai' => $this->request->getPost('nama_pegawai'),
+            'nip' => $this->request->getPost('nip'),
+            'alamat' => $this->request->getPost('alamat'),
+            'no_hp' => $this->request->getPost('no_hp')
+        ];
+           
+
+        
+
+        //insert data
+        $success = $this->model->tambah($data);
+        if ($success){
+            return redirect()->to(base_url('datapegawai'));
+        }
+
+
+    }
+
+    public function ubah()
+    {
+        $id_pegawai = $this->request->getPost('id_pegawai');
+        
+        $data = [
+            'id_pegawai' => $this->request->getPost('id_pegawai'),
+            'nama_pegawai' => $this->request->getPost('nama_pegawai'),
+            'nip' => $this->request->getPost('nip'),
+            'alamat' => $this->request->getPost('alamat'),
+            'no_hp' => $this->request->getPost('no_hp')
+        ];
+           
+        //update  data
+        $success = $this->model->ubah($data, $id_pegawai);
+        if ($success){
+            session()->setFlashdata('message', ' diubah');
+            return redirect()->to(base_url('datapegawai'));
+        }
+
+
+    }
+
+      //Hapus data
+      public function hapus()
+      {
+          $id_pegawai = $this->request->getPost('id_pegawai');
+          
+          $success = $this->model->hapus($id_pegawai);
+          if ($success){
+              session()->setFlashdata('message', ' dihapus');
+              return redirect()->to(base_url('datapegawai'));
+          }
+      }
 }
--- app/Models/M_Pegawai.php
+++ app/Models/M_Pegawai.php
@@ -14,4 +14,19 @@ class M_Pegawai extends Model{
         return $this->db->table('pegawai')->get()->getResultArray();
         
     }
+
+    public function tambah($data)
+    {
+        return $this->db->table('pegawai')->insert($data);
+    }
+
+    public function ubah($data, $id_pegawai)
+    {
+        return $this->db->table('pegawai')->update($data, ['id_pegawai' => $id_pegawai]);
+    }
+
+    public function hapus($id_pegawai)
+    {
+        return $this->db->table('pegawai')->delete(['id_pegawai' => $id_pegawai]);
+    }
 }
